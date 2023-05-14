db576830c3ee1e8cd1bae4655efd7e364e2dc9e2
khoi <khoi@khoi>
2023-05-14 15:00:48 +0700

    thêm thuộc tính cho AdstractModel

--- src/main/java/com/khoi/model/AdstractModel.java
+++ src/main/java/com/khoi/model/AdstractModel.java
@@ -1,15 +1,42 @@
 package com.khoi.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
-public class AdstractModel {
+public class AdstractModel<T> {
 	
 	private Timestamp createdDate;
 	private Timestamp modifiedDate;
 	private String createdBy;
 	private String modifiedBy;
 	private long[] ids;//dùng cho trường hợp xóa nhìu bài viết
+	private List<T> listRs;//tạo list chung cho các model
+	private Integer page;
+	private Integer maxPageItem;//tổng item tối đa trên một trang
+	private Integer toltalPage;
+	private Integer toltalItem;
+	private String sortName;
+	private String sortBy;
 	
+	public String getSortName() {
+		return sortName;
+	}
+
+
+	public void setSortName(String sortName) {
+		this.sortName = sortName;
+	}
+
+
+	public String getSortBy() {
+		return sortBy;
+	}
+
+
+	public void setSortBy(String sortBy) {
+		this.sortBy = sortBy;
+	}
+
 
 	public Timestamp getCreatedDate() {
 		return createdDate;
@@ -59,7 +86,61 @@ public class AdstractModel {
 	public void setIds(long[] ids) {
 		this.ids = ids;
 	}
-	
-	
+
+
+	public List<T> getListRs() {
+		return listRs;
+	}
+
+
+	public void setListRs(List<T> listRs) {
+		this.listRs = listRs;
+	}
+
+
+	public int getPage() {
+		return page;
+	}
+
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+
+	public Integer getMaxPageItem() {
+		return maxPageItem;
+	}
+
+
+	public void setMaxPageItem(Integer maxPageItem) {
+		this.maxPageItem = maxPageItem;
+	}
+
+
+	public Integer getToltalPage() {
+		return toltalPage;
+	}
+
+
+	public void setToltalPage(Integer toltalPage) {
+		this.toltalPage = toltalPage;
+	}
+
+
+	public Integer getToltalItem() {
+		return toltalItem;
+	}
+
+
+	public void setToltalItem(Integer toltalItem) {
+		this.toltalItem = toltalItem;
+	}
+
+
+	public void setPage(Integer page) {
+		this.page = page;
+	}
+
 	
 }
